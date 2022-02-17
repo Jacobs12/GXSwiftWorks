e5ba3e7514bc29f600283026da8355a5ed390513
fengtao92 <fengtao@ewang.com>
2022-02-17 15:34:28 +0800

    实现顶部安全区域高度的计算，状态栏高度的计算方法

--- GXSwiftWorks/Tools/GXDevice.swift
+++ GXSwiftWorks/Tools/GXDevice.swift
@@ -39,13 +39,7 @@ class GXDevice: NSObject {
     
 //    MARK: 屏幕宽高
     
-    class func screenHeight() -> CGFloat{
-        let width = UIScreen.main.bounds.size.width;
-        let height = UIScreen.main.bounds.size.height;
-        let result = max(width, height);
-        return result;
-    }
-    
+    /// 获取屏幕宽度
     class func screenWidth() -> CGFloat{
         let width = UIScreen.main.bounds.size.width;
         let height = UIScreen.main.bounds.size.height;
@@ -53,9 +47,38 @@ class GXDevice: NSObject {
         return result;
     }
     
+    /// 获取屏幕高度
+    class func screenHeight() -> CGFloat{
+        let width = UIScreen.main.bounds.size.width;
+        let height = UIScreen.main.bounds.size.height;
+        let result = max(width, height);
+        return result;
+    }
+    
+    /// 屏幕大小比例（相对于375）
     class func screenScale() -> CGFloat{
         let width = GXDevice.screenWidth();
         let result = width / 375.0;
         return result;
+    }
+    
+//    MARK: 顶部区域
+    
+    /// 顶部安全区域高度
+    class func topSafeAreaHeight() -> CGFloat{
+        var height = 0.0;
+        if(GXDevice.isHaveSafeArea() == true){
+            height = 24.0;
+        }
+        return height;
+    }
+    
+    /// 状态栏高度
+    class func stautsBarHeight() -> CGFloat{
+        var height = 20.0;
+        if(GXDevice.isHaveSafeArea() == true){
+            height = 44.0;
+        }
+        return height;
     }
 }
