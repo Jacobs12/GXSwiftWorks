fdaf373d4defda5807dba8c9f790bbc1dcf811af
fengtao92 <fengtao@ewang.com>
2022-02-17 15:11:04 +0800

    swift在项目实践中实现判断是否有安全区域，是否为刘海屏手机

---
+++ GXSwiftWorks/Tools/GXDevice.swift
@@ -0,0 +1,32 @@
+//
+//  GXDevice.swift
+//  GXSwiftWorks
+//
+//  Created by wolffy on 2022/2/17.
+//
+
+import UIKit
+
+class GXDevice: NSObject {
+    
+    class func mainWindow() -> UIWindow{
+        let window:UIWindow? = (UIApplication.shared.delegate?.window)!;
+        return window!;
+    }
+    
+    class func isHaveSafeArea() -> Bool{
+        var result = false;
+        let window:UIWindow? = GXDevice.mainWindow();
+        let bottom = (window?.safeAreaInsets.bottom)!;
+        if(bottom > 0.1){
+            result = true;
+        }
+        return result;
+    }
+
+    class func isPhoneX() -> Bool{
+        var result = GXDevice.isHaveSafeArea();
+        return result;
+    }
+    
+}
