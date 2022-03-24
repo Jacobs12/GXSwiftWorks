62ddb0675e9eae138a9acf146214210513913769
fengtao92 <fengtao@ewang.com>
2022-03-24 11:05:32 +0800

    建立AFNetworking桥接方式文件

---
+++ GXSwiftWorks/Tools/Networking/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  GXSwiftWorks
+//
+//  Created by wolffy on 2022/3/24.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+
+// 建立桥接方式：Build Settings -> Swift Compiler - General -> Objective-C Bridging Header:GXSwiftWorks/Tools/Networking/Bridge-Header.h
+#import "AFNetworking.h"
+
+#endif /* Bridge_Header_h */
