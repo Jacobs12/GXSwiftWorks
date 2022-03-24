6991ad0667c4f402ddceed42881782b5c571787b
fengtao92 <fengtao@ewang.com>
2022-03-24 10:49:52 +0800

    定义请求方式枚举类型，将请求方式类型转化为可识别字符串

--- GXSwiftWorks/Tools/Networking/GXSessionRequest.swift
+++ GXSwiftWorks/Tools/Networking/GXSessionRequest.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 enum GXRequestMode{
-    case GXRequestModeGet
-    case GXRequestModePost
-    case GXRequestModePut
-    case GXRequestModeDelete
+    case Get
+    case Post
+    case Put
+    case Delete
+    case Others
 };
 
 class GXSessionRequest: NSObject {
-
+    
+    func string(withRequest mode:GXRequestMode) -> String{
+        var result:String? = "Get";
+        switch mode{
+        case .Get:
+            result = "GET";
+            break;
+        case .Post:
+            result = "POST";
+            break;
+        case .Put:
+            result = "PUT";
+            break;
+        case .Delete:
+            result = "DELETE";
+            break;
+        default:
+            break;
+        }
+        return result!;
+    }
 }
