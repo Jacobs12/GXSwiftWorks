47c276d6057b12b1bddaa4217cdba7466d41e3fb
fengtao92 <fengtao@ewang.com>
2022-04-13 14:46:52 +0800

    实现判断是否开启系统推送方法

--- GXSwiftWorks/Tools/GXUtils.swift
+++ GXSwiftWorks/Tools/GXUtils.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class GXUtils: NSObject {
     
+    /// 获取App版本号
+    /// - Returns: App版本号
     class func appVersion() -> String{
         let key = "CFBundleShortVersionString";
         var version:String = "";
@@ -17,6 +19,8 @@ class GXUtils: NSObject {
         return version;
     }
     
+    /// 获取App的名称
+    /// - Returns: App的名称
     class func bundleDisplayName() -> String{
         let key = "CFBundleDisplayName";
         var value:String = "";
@@ -25,5 +29,35 @@ class GXUtils: NSObject {
             value = dict?[key] as! String;
         }
         return value;
+    }
+    
+    /// 判断是否开启系统推送
+    /// - Returns: 是否开启推送
+    func isRemoteNotificationEnabled() -> Bool{
+        var isEnabled = false;
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().getNotificationSettings { result in
+                switch result.authorizationStatus{
+                case .authorized:
+                    isEnabled = true;
+                    break;
+                case .denied:
+                    isEnabled = false;
+                    break;
+                default:
+                    isEnabled = false;
+                    break;
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+            let isNotificationEnabled = UIApplication.shared.currentUserNotificationSettings?.types.contains(UIUserNotificationType.alert)
+            if(isNotificationEnabled == true){
+                isEnabled = true;
+            }else{
+                isEnabled = false;
+            }
+        };
+        return isEnabled;
     }
 }
