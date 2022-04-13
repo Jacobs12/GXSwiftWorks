a33800c82d31bfa1aaf8ce68f79f49ffa92b2f23
fengtao92 <fengtao@ewang.com>
2022-04-13 15:15:24 +0800

    实现String长度获取方法

--- GXSwiftWorks/Tools/UILabel+Attributed.swift
+++ GXSwiftWorks/Tools/UILabel+Attributed.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension UILabel{
+    func set(text:String ,line spacing:CGFloat) ->Void{
+        var paragraph:NSMutableParagraphStyle = NSMutableParagraphStyle.init();
+        paragraph.lineSpacing = spacing;
+        let string:String = self.text ?? "";
+        let attributedString:NSMutableAttributedString = NSMutableAttributedString.init(string:string);
+        attributedString.addAttributes([.paragraphStyle:paragraph], range: NSMakeRange(0, string.count));
+    }
+}
