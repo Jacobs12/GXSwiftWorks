8405877b26063534b3bc380d14f3a1f337aeb363
fengtao92 <fengtao@ewang.com>
2022-04-20 16:51:22 +0800

    对webkit的WKWebViewConfiguration进行初始化配置

--- GXSwiftWorks/Class/GXWebViewViewController.swift
+++ GXSwiftWorks/Class/GXWebViewViewController.swift
@@ -11,10 +11,15 @@ import WebKit
 class GXWebViewViewController: UIViewController {
     
     var _webView:WKWebView?;
+    var config:WKWebViewConfiguration?;
     var webView:WKWebView?{
         get{
             if(_webView == nil){
-                
+                let config:WKWebViewConfiguration = WKWebViewConfiguration.init();
+                config.allowsInlineMediaPlayback = true;
+                self.config = config;
+                config.userContentController = WKUserContentController.init();
+//                self.config?.userContentController.addScriptMessageHandler(<#T##scriptMessageHandlerWithReply: WKScriptMessageHandlerWithReply##WKScriptMessageHandlerWithReply#>, contentWorld: .defaultClient, name: <#T##String#>)
             }
             return _webView;
         }
