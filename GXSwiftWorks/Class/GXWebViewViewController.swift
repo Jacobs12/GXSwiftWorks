eff706f8490e604966b0ffdb466fcc4e03579bba
fengtao92 <fengtao@ewang.com>
2022-04-20 16:21:44 +0800

    在视图控制器上定义网页控制视图WebKit

--- GXSwiftWorks/Class/GXWebViewViewController.swift
+++ GXSwiftWorks/Class/GXWebViewViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class GXWebViewViewController: UIViewController {
+    
+    var _webView:WKWebView?;
+    var webView:WKWebView?{
+        get{
+            if(_webView == nil){
+                
+            }
+            return _webView;
+        }
+    };
 
     override func viewDidLoad() {
         super.viewDidLoad()
