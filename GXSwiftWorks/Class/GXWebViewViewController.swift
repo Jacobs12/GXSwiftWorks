fe7f27bd5f6d529a715682ddc013f628caffb9a7
fengtao92 <fengtao@ewang.com>
2022-04-20 16:17:20 +0800

    使用JS代码实现获取到的用户信息传值

---
+++ GXSwiftWorks/Class/GXWebViewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GXWebViewViewController.swift
+//  GXSwiftWorks
+//
+//  Created by wolffy on 2022/4/20.
+//
+
+import UIKit
+
+class GXWebViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
