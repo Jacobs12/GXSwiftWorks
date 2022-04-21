5b009511ce9271f0e6b0a423efe0b7944b9ea94a
fengtao92 <fengtao@ewang.com>
2022-04-21 11:45:53 +0800

    在userContentController中注册"AppConfirm","getUserInformation","getUserStatus","log","shareToPlatform"等相关JS方法并解决循环引用问题

--- GXSwiftWorks/Class/GXWebViewViewController.swift
+++ GXSwiftWorks/Class/GXWebViewViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class GXScriptMessageHandlerDelegate : NSObject,WKScriptMessageHandler{
+class GXScriptMessageHandlerDelegate : NSObject,WKNavigationDelegate,WKScriptMessageHandler,WKUIDelegate {
 //    用于防止向JS中注入handler的时候强引用了self,最终导致内存泄漏 webView不销毁
     
     weak var scriptDelegate:WKScriptMessageHandler?;
@@ -22,7 +22,11 @@ class GXScriptMessageHandlerDelegate : NSObject,WKScriptMessageHandler{
     }
 }
 
-class GXWebViewViewController: UIViewController {
+class GXWebViewViewController: UIViewController,WKNavigationDelegate,WKScriptMessageHandler,WKUIDelegate {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+    }
+    
     
     var _webView:WKWebView?;
     var config:WKWebViewConfiguration?;
@@ -32,7 +36,13 @@ class GXWebViewViewController: UIViewController {
                 let config:WKWebViewConfiguration = WKWebViewConfiguration.init();
                 config.allowsInlineMediaPlayback = true;
                 self.config = config;
-                config.userContentController = WKUserContentController.init();
+                let userContentController:WKUserContentController? = WKUserContentController();
+                let nameArray:Array = ["AppConfirm","getUserInformation","getUserStatus","log","shareToPlatform"];
+                for name:String in nameArray{
+                    let handler:GXScriptMessageHandlerDelegate? = GXScriptMessageHandlerDelegate.init();
+                    handler?.set(delegate: self);
+                    userContentController?.add(handler!, name: name);
+                }
 //                self.config?.userContentController.addScriptMessageHandler(<#T##scriptMessageHandlerWithReply: WKScriptMessageHandlerWithReply##WKScriptMessageHandlerWithReply#>, contentWorld: .defaultClient, name: <#T##String#>)
                 if #available(iOS 14.0, *) {
                     let webpagePreferences:WKWebpagePreferences = WKWebpagePreferences.init();
